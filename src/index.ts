d92bb38f4225d105e7f26dc1b6ec236d771ea70e
Juan salvador Bahena <89801260+JuanSalvadorBC@users.noreply.github.com>
2022-06-28 20:43:36 -0500

    carga de los archivos del molino 3d

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,205 @@
-import { CanvasLocal } from './CanvasLocal.js';
-let canvas : HTMLCanvasElement;
-let graphics : CanvasRenderingContext2D;
+//import { Input } from './Input.js';
+import { Obj3D } from './Obj3D.js';
+//import { Canvas3D } from './Canvas3D.js';
+//import { CvWireframe } from './CvWireFrame.js';
+import { CvHLines } from './CvHLines.js';
+import { Rota3D } from './Rota3D.js';
+import { Point3D } from './Point3D.js';
 
+let canvas: HTMLCanvasElement;
+let graphics: CanvasRenderingContext2D;
+let mouse:any ={ x: null, y: null, };
 canvas = <HTMLCanvasElement>document.getElementById('circlechart');
 graphics = canvas.getContext('2d');
-/*
-let obtanch = document.getElementById("ejex");
-var cnvancho = obtanch.innerHTML;
-//var ancho = parseFloat(cnvancho);
-let obtlargo = document.getElementById("ejey");
-var cnvlargo = obtlargo.innerHTML;
-//var largoo =  parseFloat(cnvlargo);
-*/
-const obtx = <HTMLInputElement>document.querySelector('.ejex');
-const obty = <HTMLInputElement>document.querySelector('.ejey');
-//variables para el efecto
+let band=true;
+let cv: CvHLines;
+let obj: Obj3D;
+let ang: number=0;
 
-let scale=1;
-let b = false;
-let lnz = document.getElementById("zom");
-lnz.onwheel = zoomm;
-
-
-
-function zoomm(e:any):void{   
-  e.preventDefault();
-  scale += e.deltaY * -0.01;
-  scale = Math.min(Math.max(0.2, scale), 4);
-  lnz.style.transform = `scale(${scale})`;
-
-}
-
-
-function entrada()
-{
-  reset();
-  let cx = parseInt(obtx.value);
-    let cy = parseInt(obty.value);
-  if( obtx.value==='' && obty.value==='')
-  {
-    mapa();
-    cx=cx;
-    cy=cy;
-  }else{
-    obtx.value="";
-    obty.value="";
-    const miCanvas:CanvasLocal = new CanvasLocal(graphics, canvas, cx, cy);
-      miCanvas.paint();
+function leerArchivo(e:any) {
+  var archivo = e.target.files[0];
+  if (!archivo) {
+    return;
   }
+  var lector = new FileReader();
+  lector.onload = function(e) {
+    var contenido = e.target.result;
+    mostrarContenido(contenido);
+    obj = new Obj3D();
+    if (obj.read(contenido)) {
+      //sDir = sDir1;
+      cv = new CvHLines(graphics, canvas);
+      cv.setObj(obj);
+      cv.paint();
+    }
+  };
+  lector.readAsText(archivo);
 }
-function reset(){
-    graphics.clearRect(0, 0, canvas.width, canvas.height);
+
+function mostrarContenido(contenido:any) {
+  var elemento = document.getElementById('contenido-archivo');
+  //
+  //readObject(new Input(contenido));
+  elemento.innerHTML = contenido;
 }
-function mapa(){
-    let a = 8;
-    let l = 8;
-    const miCanvas : CanvasLocal = new CanvasLocal(graphics, canvas, l, a);
-    miCanvas.paint(); 
+
+function vp(dTheta:number, dPhi:number, fRho:number):void{  // Viewpoint
+  if (obj != undefined) {
+    let obj: Obj3D = cv.getObj();
+    if (!obj.vp(cv, dTheta, dPhi, fRho))
+      alert('datos no validos');
+  }
+  else
+    alert('aun no has leido un archivo');
 }
-mapa();
+
+function eyeDownFunc() {
+  vp(0, 0.1, 1);
+}
+
+function eyeUpFunc() {
+  vp(0, -0.1, 1);
+}
+
+function eyeLeftFunc() {
+  vp(-0.1, 0, 1);
+}
+
+function eyeRightFunc() {
+  vp(0.1, 0, 1);
+}
+
+function incrDistFunc() {
+  vp(0, 0, 2);
+}
+
+function decrDistFunc() {
+  vp(0, 0, 0.5);
+}
+
+function pza1DerFunc() {
+  let af = 10;
+ 	
+	Rota3D.initRotate( obj.w[139], obj.w[138], af*Math.PI/180);	
+	
+  for (let i = 140; i <= 164; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
+	}
+	cv.setObj(obj);
+  cv.paint();	
+}
+
+function pza1IzqFunc() {
+  let af = -10;
+ 	
+	Rota3D.initRotate( obj.w[138], obj.w[139], af*Math.PI/180);	
+	
+  for (let i = 140; i <= 164; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
+	}
+	cv.setObj(obj);
+  cv.paint();	
+}
+function pza12DerFunc() {
+  let af = 10;
+  Rota3D.initRotate(obj.w[165], obj.w[97], af * Math.PI / 180);
+	
+  for (let i = 98; i <= 164; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
+  }
+ 
+	cv.setObj(obj);
+  cv.paint();	
+}
+
+function pza12IzqFunc() {
+  let af = -10;
+	Rota3D.initRotate( obj.w[165], obj.w[97], af*Math.PI/180);	
+	
+  for (let i = 98; i <= 164; i++){
+    obj.w[i] = Rota3D.rotate(obj.w[i]);
+	}
+  
+	cv.setObj(obj);
+  cv.paint();	
+}
 
 
-// document.getElementById("agzoom").addEventListener('click',zoom, false);
-document.getElementById('btnz').addEventListener('click',entrada, false);
+document.getElementById('file-input').addEventListener('change', leerArchivo, false);
+document.getElementById('eyeDown').addEventListener('click', eyeDownFunc, false);
+document.getElementById('eyeUp').addEventListener('click', eyeUpFunc, false);
+document.getElementById('eyeLeft').addEventListener('click', eyeLeftFunc, false);
+document.getElementById('eyeRight').addEventListener('click', eyeRightFunc, false);
+document.getElementById('incrDist').addEventListener('click', incrDistFunc, false);
+document.getElementById('decrDist').addEventListener('click', decrDistFunc, false);
+
+
+//movimiento de piezas
+document.getElementById('pza1Izq').addEventListener('click', pza1IzqFunc, false);
+document.getElementById('pza1Der').addEventListener('click', pza1DerFunc, false);
+document.getElementById('pza12Izq').addEventListener('click', pza12IzqFunc, false);
+document.getElementById('pza12Der').addEventListener('click', pza12DerFunc, false);
+
+
+let Pix: number, Piy: number;
+let Pfx: number, Pfy: number;
+let theta = 0.3, phi = 1.3, SensibilidadX = 0.02, SensibilidadY = 0.02;
+let flag: boolean = false;
+
+function handleMouse(evento: any) {
+  Pix=evento.offsetX;
+  Piy = evento.offsetY;
+  flag = true;
+}
+
+function makeVizualization(evento: any) {
+  if (flag) {
+    Pfx = evento.offsetX;
+    Pfy = evento.offsetY;
+    //console.log(Pfx, Pfy)
+    let difX = Pix - Pfx;
+    let difY = Pfy - Piy;
+    vp(0, 0.1 * difY / 50, 1);
+    Piy = Pfy;
+    vp(0.1 * difX, 0 / 50, 1);
+    Pix = Pfx;
+    /*if( Piy>Pfy+1 ){
+      phi += SensibilidadY;
+      vp(0, 0.1*, 1);
+      //cv.redibuja(theta, phi, tamanoObjeto);
+      Piy=Pfy;
+    }
+
+    if(Pfy>Piy+1){
+      phi -= SensibilidadY;
+      vp(0,-0.1, 1);
+      //cv.redibuja(theta, phi, tamanoObjeto);
+      Piy=Pfy;
+    }*/
+
+    /*if (Pix > Pfx + 1) {
+      theta += SensibilidadX;
+      vp(0.1, 0, 1);
+      //cv.redibuja(theta, phi, tamanoObjeto);
+      Pix = Pfx;
+    }
+        
+    if (Pfx > Pix + 1) {
+      theta -= SensibilidadX;
+      vp(-0.1, 0, 1);
+      //cv.redibuja(theta, phi, tamanoObjeto);
+      Pix = Pfx;
+
+  */
+
+      }
+}
+    }
+
+function noDraw() {
+  flag = false;
+}
+
+canvas.addEventListener('mousedown', handleMouse);
+canvas.addEventListener('mouseup', noDraw);
+//canvas.addEventListener('mousemove', makeVizualization);
